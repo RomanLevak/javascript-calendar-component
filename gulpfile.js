d2e730cac6850052cd8be1691aecbde0ce18429f
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2018-12-21 08:38:12 +0530

    gulp update to 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,34 @@
-const gulp = require('gulp')
-const less = require('gulp-less')
-const concat = require('gulp-concat')
-const sourcemaps = require('gulp-sourcemaps')
-const autoprefixer = require('gulp-autoprefixer')
-const cleanCss = require('gulp-clean-css')
-const browserSync = require('browser-sync').create()
+const gulp = require("gulp");
+const less = require("gulp-less");
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCss = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
 
-gulp.task('less', () =>
-	gulp
-		.src('./styles/*.less')
-		.pipe(sourcemaps.init())
-		.pipe(autoprefixer())
-		.pipe(less())
-		.pipe(concat('calendar.css'))
-		.pipe(cleanCss())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('styles'))
-		.pipe(browserSync.stream())
-)
+gulp.task("less", () => {
+  gulp
+    .src("./styles/*.less")
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    .pipe(less())
+    .pipe(concat("calendar.css"))
+    .pipe(cleanCss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("styles"))
+	.pipe(browserSync.stream())
+});
 
-gulp.task('serve', () => {
-	browserSync.init({
-		server: {
-			baseDir: './'
-		}
-	})
-	gulp.watch('./styles/*.less', ['less'])
-	gulp.watch('./index.html').on('change', browserSync.reload)
-})
+gulp.task("serve", () => {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./styles/*.less", gulp.series("less"));
+  gulp.watch("./index.html").on('change', () => {
+	browserSync.reload();
+  });
+});
 
-gulp.task('default', ['less', 'serve'])
+gulp.task('default', gulp.series(gulp.parallel('less', 'serve'), function () {}))
